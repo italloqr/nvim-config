99cbdda10b0af6b2c254df76f7a3dac64f97ee43
Itallo Rodrigues <italloqr@gmail.com>
2024-06-28 07:39:11 -0300

    feat: add debugging

--- lua/itallo/remap.lua
+++ lua/itallo/remap.lua
@@ -7,16 +7,14 @@ vim.keymap.set("n", "<C-b>", vim.cmd.Neotree)
 --vim.keymap.set("n", "<C-d>", "<C-d>zz")
 --vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
---vim.keymap.set("i", "<C-c>", "<Esc>")
+vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.keymap.set("n", "tr", ":bp<CR>")
 vim.keymap.set("n", "ty", ":bn<CR>")
 vim.keymap.set("n", "td", ":bd<CR>")
 
---vim.keymap.set("n", "<leader><leader>", function()
---    vim.cmd("so")
---end)
 vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>gd", vim.cmd.DiffviewOpen)
+
 -- Comment.nvim
 -- `gcc` - Toggles the current line using linewise comment
 -- [[ `gbc` - Toggles the current line using blockwise comment ]]
@@ -35,3 +33,16 @@ vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 vim.keymap.set("n", "<leader>ft", vim.lsp.buf.format, {})
+
+
+-- nvim-surround
+--    Old text                    Command         New text
+--------------------------------------------------------------------------------
+    -- surr*ound_words             ysiw)           (surround_words)
+    -- *make strings               ys$"            "make strings"
+    -- [delete ar*ound me!]        ds]             delete around me!
+    -- remove <b>HTML t*ags</b>    dst             remove HTML tags
+    -- 'change quot*es'            cs'"            "change quotes"
+    -- <b>or tag* types</b>        csth1<CR>       <h1>or tag types</h1>
+    -- delete(functi*on calls)     dsf             function calls
+
